8305d008b252533d81b2c46ddf36fd4a0308900a
Gankova Lidiya <gankoval.a.0108@mail.ru>
2023-03-27 22:53:11 +0300

    Added Task_2

---
+++ Task_2.java
@@ -0,0 +1,36 @@
+// Task_2.
+// Вводится число n, затем n чисел целых, по одному на каждой строке. Затем вводится число, на которое нужно умножить все введённые выше числа. 
+// Выведите на экран результат умножения построчно.
+// Sample Input:
+// 4
+// 1
+// 2
+// 3
+// 5
+// 2
+// Sample Output:
+// 2
+// 4
+// 6
+// 10
+
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i ++) {
+            System.out.print("Введите число: ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Введите множитель: ");
+        int num = sc.nextInt();
+        for (int i : arr) {
+            System.out.println(i * num);
+        }
+        sc.close();
+    }
+}
